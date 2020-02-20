bfad1db983f691a1e53ec3d5037d0d699c66b6e5
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-02-20 00:26:12 -0500

    reupload html1 and js1

--- Basic JS Projects/basic_js_1.js
+++ Basic JS Projects/basic_js_1.js
@@ -1,4 +1,16 @@
+window.alert("Hello world");
+document.write("Hello World! <br>");
+
+var str = "Javascript is so cool.";
+
+document.write(str + "<br>");
+window.alert(str);
+document.write("\"Do or do not");
+document.write(", " + "there is no try\"" + "-Yoda <br>" );
+
 var a = "apple", b = "banana", c = "carrot";
 var a = a.fontcolor("red"), b = b.fontcolor("yellow"), c = c.fontcolor("orange");
-document.write(a + b + c);
+document.write(a +" "+ b + " "+ c + "<br>");
+
+document.write(10+10);
 
