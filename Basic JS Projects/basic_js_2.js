d30529aed0d433eb670584d107a5bb9ee0dbce68
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-02-19 22:56:06 -0500

    JS with buttons

---
+++ Basic JS Projects/basic_js_2.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str="Blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function Concatenate() {
+    var sen = "I love";
+    sen += " coding!";
+    document.getElementById("bText").innerHTML = sen;
+}
